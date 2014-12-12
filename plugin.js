28b3dcf712d7514dbf7c628a1af544a3385bd1a1
Chris Stivers <chris@aerospike.com>
2014-12-12 12:07:12 -0800

    added dashboards

--- plugin.js
+++ plugin.js
@@ -28,5 +28,13 @@ module.exports = {
     command:      "node . --interval $(pollInterval)",
     postExtract:  "npm install",
     ignore:       "node_modules",
-    metrics:      _.keys(metrics)
+    metrics:      _.keys(metrics),
+    dashboards: [
+      { name: "Aerospike Overview",
+        layout: "d-w=3&d-h=3&d-pad=5&d-bg=none&d-sg-cpu=0-0-1-1&d-sg-ni=1-0-1-1-t&d-sg-no=1-0-1-1-b&d-sg-hdrb=2-0-1-1-t&d-sg-hdwb=2-0-1-1-b&d-g-AEROSPIKE_MEM_TOTAL=0-1-1-1-t&d-g-AEROSPIKE_MEM_USED=0-1-1-1-b&d-g-AEROSPIKE_MEM_USED_INDEX=1-1-1-1-t&d-g-AEROSPIKE_MEM_USED_DATA=1-1-1-1-b&d-g-AEROSPIKE_OBJECTS=2-1-1-1-t&d-g-AEROSPIKE_OBJECTS_EVICTED=2-1-1-1-b&d-g-AEROSPIKE_CLIENTS=0-2-1-1&d-g-AEROSPIKE_PROXIES=2-2-1-1&d-g-AEROSPIKE_TXNS=1-2-1-1"
+      },
+      { name: "Aerospike Transactions",
+        layout: "d-w=3&d-h=3&d-pad=5&d-bg=none&d-g-AEROSPIKE_TXNS=0-1-1-1&d-g-AEROSPIKE_OBJECTS=1-0-1-1&d-g-AEROSPIKE_CLIENTS=0-0-1-1&d-g-AEROSPIKE_MEM_USED=2-0-1-1&d-g-AEROSPIKE_READ_1MS=2-1-1-1-t&d-g-AEROSPIKE_READS_8MS=2-1-1-1-b&d-g-AEROSPIKE_WRITE_1MS=2-2-1-1-t&d-g-AEROSPIKE_WRITE_8MS=2-2-1-1-b&d-g-AEROSPIKE_PROXIES=0-2-1-1&d-g-AEROSPIKE_WRITES=1-2-1-1-t&d-g-AEROSPIKE_WRITES_ERR=1-2-1-1-b&d-g-AEROSPIKE_READS=1-1-1-1-t&d-g-AEROSPIKE_READS_ERR=1-1-1-1-b"
+      }
+    ]
 };
